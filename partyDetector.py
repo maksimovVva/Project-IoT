e0994ade8fbe1e9352a8e9edae4933a89a5b3644
maksimovVva <32110810+maksimovVva@users.noreply.github.com>
2018-02-28 13:16:34 +0300

    try

--- partyDetector.py
+++ partyDetector.py
@@ -1,59 +1,66 @@
+from flask import Flask
+import json
 import Edison
 
+app = Flask(__name__)
 edison = Edison.Edison()
 
 # light sensor, sound sensor, vibration sensor
 
 status = ""
 
-while(True):
-    while(edison.getButtonStatus() == 1):
-        if (not edison.isParty()):
-            pass
-        else:
-            while(edison.isParty()):
-                edison.displayAlarm()
-            edison.setBuzzerOff()
-            edison.clearScreen()
+if __name__ == "__main__":
+    app.run(host= '0.0.0.0')
+    print("muy")
 
-    if (status == "light"):
-        while(edison.getButtonStatus() == 0):
-            if(not edison.isParty()):
-                edison.displayLight()
-            else:
-                while (edison.isParty()):
-                    edison.displayAlarm()
-                edison.setBuzzerOff()
-        status = "sound"
-
-    elif (status == "sound"):
-        while(edison.getButtonStatus() == 0):
-            if (not edison.isParty()):
-                edison.displaySound()
-            else:
-                while (edison.isParty()):
-                    edison.displayAlarm()
-                edison.setBuzzerOff()
-        status = "vibration"
-
-    elif (status == "vibration"):
-        while(edison.getButtonStatus() == 0):
-            if (not edison.isParty()):
-                edison.displayVibration()
-            else:
-                while (edison.isParty()):
-                    edison.displayAlarm()
-                edison.setBuzzerOff()
-        status = "light"
-
-    else:
-        while(edison.getButtonStatus() == 0):
-            if (not edison.isParty()):
-                pass
-            else:
-                while (edison.isParty()):
-                    edison.displayAlarm()
-                edison.setBuzzerOff()
-                edison.clearScreen()
-        status = "light"
+# while(True):
+#     while(edison.getButtonStatus() == 1):
+#         if (not edison.isParty()):
+#             pass
+#         else:
+#             while(edison.isParty()):
+#                 edison.displayAlarm()
+#             edison.setBuzzerOff()
+#             edison.clearScreen()
+#
+#     if (status == "light"):
+#         while(edison.getButtonStatus() == 0):
+#             if(not edison.isParty()):
+#                 edison.displayLight()
+#             else:
+#                 while (edison.isParty()):
+#                     edison.displayAlarm()
+#                 edison.setBuzzerOff()
+#         status = "sound"
+#
+#     elif (status == "sound"):
+#         while(edison.getButtonStatus() == 0):
+#             if (not edison.isParty()):
+#                 edison.displaySound()
+#             else:
+#                 while (edison.isParty()):
+#                     edison.displayAlarm()
+#                 edison.setBuzzerOff()
+#         status = "vibration"
+#
+#     elif (status == "vibration"):
+#         while(edison.getButtonStatus() == 0):
+#             if (not edison.isParty()):
+#                 edison.displayVibration()
+#             else:
+#                 while (edison.isParty()):
+#                     edison.displayAlarm()
+#                 edison.setBuzzerOff()
+#         status = "light"
+#
+#     else:
+#         while(edison.getButtonStatus() == 0):
+#             if (not edison.isParty()):
+#                 pass
+#             else:
+#                 while (edison.isParty()):
+#                     edison.displayAlarm()
+#                 edison.setBuzzerOff()
+#                 edison.clearScreen()
+#         status = "light"
 
